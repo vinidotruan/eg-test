5700f347eb1e5acddfe301781bea553eddf6eba0
vinidotruan <contatoruanvinicius@gmail.com>
2025-02-21 17:27:32 -0300

    add requests and security rules

---
+++ app/Http/Requests/Step/StoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Step;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if(!$this->route('id')) {
+            return false;
+        }
+        return Auth::user()->id === (int) $this->route('id');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
